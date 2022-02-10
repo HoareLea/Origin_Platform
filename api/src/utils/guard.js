2b72dae4cde6ce675d035fe150c91b1f64e134b4
Anna Kampani <52359614+anna-kampani@users.noreply.github.com>
2022-02-10 18:47:52 +0000

    Fix/ remove tests

--- api/src/utils/guard.js
+++ api/src/utils/guard.js
@@ -16,31 +16,31 @@ const routeGuard = async (req, res, next) => {
 
     } else {
         const groups = decoded.groups;
-        //req.user = decoded;
+        
 
         // Get access control - compare with config
         if (req.path.includes(config.accessMatrix.graphql.path)) {
             if (config.accessMatrix.graphql.methods.includes(req.method)) {
 
-                
+                req.user = decoded;
 
                 // Check: group ids to match with allowed groups / Currently not in use
-                let intersection = config.accessMatrix.graphql.groups
-                    .filter(group => groups.includes(group.uuid));
+                // let intersection = config.accessMatrix.graphql.groups
+                //     .filter(group => groups.includes(group.uuid));
 
-                if (intersection.length < 1) {
-                    return res.status(403).json({ error: 'User does not have the group' });
-                }
-                else {
-                    // Add user & roles
-                    req.user = decoded;
-                    req.user.roles = intersection;
-                    // Prioritise auth clearance
-                    let auth = intersection.map(x=>x.role)
-                    if(auth.includes("Admin"))req.user.auth="Admin"
-                    else if(auth.includes("Member"))req.user.auth="Member"
-                    else req.user.auth="Viewer"
-                }
+                // if (intersection.length < 1) {
+                //     return res.status(403).json({ error: 'User does not have the group' });
+                // }
+                // else {
+                //     // Add user & roles
+                //     req.user = decoded;
+                //     req.user.roles = intersection;
+                //     // Prioritise auth clearance
+                //     let auth = intersection.map(x=>x.role)
+                //     if(auth.includes("Admin"))req.user.auth="Admin"
+                //     else if(auth.includes("Member"))req.user.auth="Member"
+                //     else req.user.auth="Viewer"
+                // }
             } else {
                 return res.status(403).json({ error: 'Method not allowed' });
             }
